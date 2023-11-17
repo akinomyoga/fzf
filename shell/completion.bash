686c3bd62ce990119065590ca2e6341d9470e668
Koichi Murase <myoga.murase@gmail.com>
2023-11-17 15:24:01 +0900

    [bash] Specify -a to an array declaration

--- shell/completion.bash
+++ shell/completion.bash
@@ -472,7 +472,7 @@ _fzf_proc_completion_post() {
 #   # Use bash-completions’s _known_hosts_real() for getting the list of hosts
 #   __fzf_list_hosts() {
 #     # Set the local attribute for any non-local variable that is set by _known_hosts_real()
-#     local COMPREPLY=()
+#     local -a COMPREPLY=()
 #     _known_hosts_real ''
 #     printf '%s\n' "${COMPREPLY[@]}" | command sort -u --version-sort
 #   }
